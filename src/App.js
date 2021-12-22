625aca9bf28f046dfa01c4b1a1bbdd78a6c0cfd1
CrystalSchurko <cschurko@banyanhill.com>
2021-12-22 10:47:01 -0500

    baseline setup for app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+import Accordion from './components/Accordion';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end js framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use react by creating reusable components'
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <Accordion items={items} />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default App;
